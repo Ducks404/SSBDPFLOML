28a6a01037aed9c31d3f2fa2b270ab1a73e1faa5
Ducks404 <aghakoeswantoro@gmail.com>
2023-07-10 14:36:33 +0700

    Only one page at top at most

--- login.js
+++ login.js
@@ -8,3 +8,25 @@ function validate(form) {
     }
     return false
 }
+
+let activeIndex=0;
+
+const pages = document.getElementsByClassName("page");
+
+const handleFlipPage = () => {
+
+    // Move shown up
+    const currentPage = document.querySelector(`[data-index="${activeIndex}"]`)
+    currentPage.dataset.status = 'after';
+    
+    // Move not shown down
+    const prevIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : 7;
+
+    const prevPage = document.querySelector(`[data-index="${prevIndex}"]`);
+
+    prevPage.dataset.status = 'shown';
+
+    // Update activeIndex
+
+    activeIndex = activeIndex + 1 <= pages.length - 1 ? activeIndex + 1 : 0;
+}
